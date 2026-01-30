60af2fbc6ada4696151cdf65bf7a090ad23e0d1e
houyu <houyu@cocos.com>
2026-01-30 10:26:01 +0800

    no message

--- vars/cocosPipeline.groovy
+++ vars/cocosPipeline.groovy
@@ -44,7 +44,8 @@ def call(ctx) {
             ${ctx.params.mode} ^
             ${ctx.env.CREATOR_PATH} ^
             ${ctx.params.apk} ^
-            ${ctx.params.clean}
+            ${ctx.params.bundleName} ^
+            ${ctx.params.apk}
         """
     }
 }
