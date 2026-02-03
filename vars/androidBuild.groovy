f1deddf0b66d1124d7a7f42cf3295c5f440c35d4
houyu <houyu@cocos.com>
2026-02-03 14:23:03 +0800

    no message

--- vars/androidBuild.groovy
+++ vars/androidBuild.groovy
@@ -3,14 +3,17 @@ def call(ctx) {
     def (versionName, versionCode) =
         org.cocos.utils.AndroidUtils.resolveVersion(this, ctx.params, ctx.env.WORKSPACE)
 
-    versionName =  ctx.params.versionName ?: versionName
-    versionCode = ctx.params.versionCode ?: versionCode
+    def new_versionName = ctx.params.versionName ?: versionName
+    def new_versionCode = ctx.params.versionCode ?: versionCode
+
+    ctx.env.android_version_name =  new_versionName
+    ctx.env.android_version_code = new_versionCode
 
     dir('build/android/proj') {
         bat """
         gradlew assemble${ctx.params.channel.capitalize()}${ctx.params.mode.capitalize()} \
-        -PversionName=${versionName} \
-        -PversionCode=${versionCode}
+        -PversionName=${new_versionName} \
+        -PversionCode=${new_versionCode}
         """
     }
 }
